43ae03fba0754f2ef48d251c06ab92c2b65423af
campagno <campagno.eduardo@outlook.com>
2020-03-05 19:12:18 -0300

    operacionesCallback OK

--- operacionesCallbacks.js
+++ operacionesCallbacks.js
@@ -1,3 +1,6 @@
+
+//En este caso unaFuncion seria nuestro callback
+
 function cambia(x, unaFuncion) {
     z = x + 1
     y = unaFuncion(z)
@@ -21,14 +24,17 @@ valor = 2 //no se puede modificar
 
 
 // que el valor de res1 sea 15
-var res1 = //cambia(...completar)
+var res1 = cambia(valor,porCinco)
 console.log(res1)
 
 
 // que el valor de res2 sea 30
-var res2 = //cambia(...completar)
+var res2 = cambia(valor, porDiez)
 console.log(res2)
 
 
 var res3 = cambia(cambia(valor,porCinco),porDiez)
+//Vale 160 porque 
+//  cambia(valor,porCinco) => 15
+//  cambia(15,porDiez) => (15 + 1) * 10
 // ¿Cuánto vale res3 acá? No vale hacer console.log ni nada por el estilo.
